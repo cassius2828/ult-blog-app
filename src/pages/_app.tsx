e7ccdb1116a71a3043a26953f5a9cb382793f7a5
cassiusreynolds <cassius@syncsmart.io>
2025-06-26 22:37:46 -0700

    added context provider around app file content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { type AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import GlobalContextProvider from "../_context";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +13,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <GlobalContextProvider>
+        <Component {...pageProps} />
+      </GlobalContextProvider>
     </SessionProvider>
   );
 };
